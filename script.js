4d0ed033fea9219ed727c8ecde4e242f5cf82baf
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-01-05 04:54:42 -0300

    criando lógica

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+let dados = {};
+
+//! Carregando dados do arquivo JSON e o atribuido a variável dados -----------
+
 fetch('./data.json')
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => {
+    dados = json;
+    comecaCodigo();
+  });
+
+//!começando o código -------------------------------------------------------
+
+function comecaCodigo() {
+  //! Fazendo a seleção dos elementos -----------------
+  const cardsTrending = document.querySelectorAll('.trending_card');
+  const yearsOfTrending = document.querySelectorAll('.year');
+  // console.log(dados);
+
+  //todo --  cria array dos itens que estão na categoria trending. ----
+  let trending = [];
+  dados.map((item) => {
+    if (item.isTrending) {
+      trending.push(item);
+    }
+  });
+  console.log(trending);
+  //todo -- ---------------------------------
+  console.log(cardsTrending);
+  for (let i = 0; i < trending.length; i++) {
+    cardsTrending[
+      i
+    ].style.backgroundImage = `url('${trending[i].thumbnail.trending.small}')`;
+    yearsOfTrending[i].textContent = `${trending[i].year}`;
+  }
+  // cardsTrending.forEach((card, index) => {
+  //   card.style.backgroundImage = `url('${trending[index].thumbnail.trending.small}')`;
+  // });
+
+  //! Começando o código -------------
+}
