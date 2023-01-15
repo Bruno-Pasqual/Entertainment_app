6ed36cddb70799c0e236a5e717d0ed3100fd6f24
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-01-14 23:48:31 -0300

    container trending ok

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,62 @@ let dados = {};
 fetch('./data.json')
   .then((response) => response.json())
   .then((json) => {
+    localStorage.setItem('dados', JSON.stringify(dados));
     dados = json;
-    let paginaAtiva = JSON.parse(localStorage.getItem('paginaAtiva'));
+    console.log(dados);
     comecaCodigo();
   });
 
 function comecaCodigo() {
-  console.log(dados);
+  //! Selecionando os primeiros elementos ----------------------------------
+  const containerCardsEmAlta = document.getElementById(
+    'container_cards_em_alta'
+  );
+
+  //! Variáveis ------------------------------------------------------------
+  let cardsEmAlta = dados.filter((elemento, i) => {
+    //todo --- Filter que irá criar um array contendo apenas os elementos com a propriedade "isTrending"
+    if (elemento.isTrending) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  criaElementosFilhos(containerCardsEmAlta, cardsEmAlta);
+
+  //! Fim da função começa código --
+}
+
+function criaElementosFilhos(pai, array) {
+  array.map((elemento) => {
+    pai.innerHTML += ` <div class="card_em_alta">
+    <div class="bookmark_container">
+      <img
+        src="./assets/icon-bookmark-empty.svg"
+        alt=""
+        class="bookmark_icon"
+      />
+    </div>
+    <div class="container_informacoes">
+      <div class="informacoes_details">
+        <div class="first_row">
+          <p class="card_year">2019</p>
+          <ul>
+            <li>
+              <img
+                src="./assets/icon-category-movie.svg"
+                alt=""
+                class="category_icon"
+              />
+              <p class="category_name">Movie</p>
+            </li>
+            <li><p class="classification">PG</p></li>
+          </ul>
+        </div>
+        <p class="card_title">Beyond Earth</p>
+      </div>
+    </div>
+  </div>`;
+  });
 }
