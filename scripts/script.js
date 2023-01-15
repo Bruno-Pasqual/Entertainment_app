15e1f5622a7ea74977f64ae547a80f594fc6d152
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-01-15 02:57:31 -0300

    funções criaCards

--- scripts/script.js
+++ scripts/script.js
@@ -17,25 +17,83 @@ function comecaCodigo() {
   const containerCardsEmAlta = document.getElementById(
     'container_cards_em_alta'
   );
+  const containerCardsRecomendados = document.getElementById(
+    'container_cards_recomendados'
+  );
 
   //! Variáveis ------------------------------------------------------------
-  let cardsEmAlta = dados.filter((elemento, i) => {
-    //todo --- Filter que irá criar um array contendo apenas os elementos com a propriedade "isTrending"
-    if (elemento.isTrending) {
-      return true;
-    } else {
-      return false;
-    }
+  let arrayCardsEmAlta = [];
+  let arrayCardsRecomendados = [];
+  //! Variáveis ------------------------------------------------------------
+
+  dados.forEach((elemento) => {
+    elemento.isTrending
+      ? arrayCardsEmAlta.push(elemento)
+      : arrayCardsRecomendados.push(elemento);
   });
 
-  criaElementosFilhos(containerCardsEmAlta, cardsEmAlta);
+  // Argumentos (elemento pai, array)
+  criaCardsRecomendados(containerCardsEmAlta, arrayCardsEmAlta);
+  criaCardsGenericos(containerCardsRecomendados, arrayCardsRecomendados);
 
   //! Fim da função começa código --
 }
 
-function criaElementosFilhos(pai, array) {
-  array.map((elemento) => {
-    pai.innerHTML += ` <div class="card_em_alta">
+function criaCardsRecomendados(pai, array) {
+  //todo -- Função que irá receber 2 parâmetros, o elemento pai e o array que será usado para criar os elementos e também as informações que irão alterar as informações dos mesmos.
+
+  // let cards = ['card_uso_geral', 'card_em_alta'];
+  console.log(pai.classList.contains('container_cards_em_alta'));
+
+  array.map((elemento, index) => {
+    //Criando os cards de acordo com a quantidade e com as informações dos objetos dentro do array.
+
+    pai.innerHTML += ` <div class="${
+      elemento.isTrending === true ? 'card_em_alta' : 'card_uso_geral'
+    }">
+      <div class="bookmark_container">
+        <img
+          src="./assets/icon-bookmark-empty.svg"
+          alt=""
+          class="bookmark_icon"
+        />
+      </div>
+      <div class="container_informacoes">
+        <div class="informacoes_details">
+          <div class="first_row">
+            <p class="card_year">${elemento.year}</p>
+            <ul>
+              <li>
+                <img
+                  src="./assets/icon-category-${
+                    elemento.category === 'Movie' ? 'movie' : 'tv'
+                  }.svg"
+                  alt=""
+                  class="category_icon"
+                />
+                <p class="category_name">${elemento.category}</p>
+              </li>
+              <li><p class="classification">${elemento.rating}</p></li>
+            </ul>
+          </div>
+          <p class="card_title">${elemento.title}</p>
+        </div>
+      </div>
+    </div>`;
+
+    //Selecionando os cards criados e alterando a sua imagem de fundo
+    if (elemento.isTrending === true) {
+      const cardsEmAlta = document.querySelectorAll('.card_em_alta');
+      cardsEmAlta[
+        index
+      ].style.backgroundImage = `url("${elemento.thumbnail.trending.small}")`;
+    }
+  });
+}
+
+function criaCardsGenericos(pai, array) {
+  array.map((elemento, index) => {
+    pai.innerHTML += `<div class="card_uso_geral">
     <div class="bookmark_container">
       <img
         src="./assets/icon-bookmark-empty.svg"
@@ -43,24 +101,23 @@ function criaElementosFilhos(pai, array) {
         class="bookmark_icon"
       />
     </div>
-    <div class="container_informacoes">
-      <div class="informacoes_details">
-        <div class="first_row">
-          <p class="card_year">2019</p>
-          <ul>
-            <li>
-              <img
-                src="./assets/icon-category-movie.svg"
-                alt=""
-                class="category_icon"
-              />
-              <p class="category_name">Movie</p>
-            </li>
-            <li><p class="classification">PG</p></li>
-          </ul>
-        </div>
-        <p class="card_title">Beyond Earth</p>
+    <div class="container_imagem"></div>
+    <div class="container_informacoes_recomendado">
+      <div class="first_row">
+        <p class="card_year">${elemento.year}</p>
+        <ul>
+          <li>
+            <img
+              src="./assets/icon-category-movie.svg"
+              alt=""
+              class="category_icon"
+            />
+            <p class="category_name">${elemento.category}</p>
+          </li>
+          <li><p class="classification">${elemento.rating}</p></li>
+        </ul>
       </div>
+      <p class="card_title">${elemento.title}</p>
     </div>
   </div>`;
   });
