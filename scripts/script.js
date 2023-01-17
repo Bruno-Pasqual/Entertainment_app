cdb3e95dc911f9a3905b9f8d744c47b3c4435eda
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-01-17 05:16:09 -0300

    trabalhando na lógica bookmark

--- scripts/script.js
+++ scripts/script.js
@@ -23,22 +23,24 @@ if (sessionStorage.getItem('dados')) {
 //! Carregando dados do arquivo JSON e o atribuido a variável dados -----------
 
 function comecaCodigo() {
-  puxaDadosEAtualiza();
+  atualizaTrendingERecomendados();
   criaOsCards();
+  selecionaosBookmark();
+
   //----------------------
 }
 
-function puxaDadosEAtualiza() {
+function atualizaTrendingERecomendados() {
   dados = JSON.parse(sessionStorage.getItem('dados'));
-  console.log(dados);
+  // console.log(dados);
   dados.map((elemento) => {
     elemento.isTrending
       ? arrayTrending.push(elemento)
       : arrayRecomendados.push(elemento);
   });
 
-  console.log(arrayTrending);
-  console.log(arrayRecomendados);
+  // console.log(arrayTrending);
+  // console.log(arrayRecomendados);
 }
 
 function criaOsCards() {
@@ -87,7 +89,7 @@ function criaOsCards() {
   //!Criando os cards em Alta ---
 
   //!Criando os cards Recomendados ---
-  arrayRecomendados.map((elemento, index) => {
+  arrayRecomendados.map((card, index) => {
     paiCardsRecomendados.innerHTML += `<div class="card_resultado">
     <div class="bookmark_container">
       <img src="./assets/icon-bookmark-empty.svg" alt="" class="bookmark_icon" />
@@ -95,7 +97,7 @@ function criaOsCards() {
     <div class="container_imagem"></div>
     <div class="container_informacoes_recomendado">
       <div class="first_row">
-        <p class="card_year">2019</p>
+        <p class="card_year">${card.year}</p>
         <ul>
           <li>
             <img
@@ -121,4 +123,48 @@ function criaOsCards() {
   });
 
   //!Criando os cards Recomendados ---
+}
+
+function selecionaosBookmark() {
+  //Selecionando os containers que estão na tela
+  let containersBookmark = document.querySelectorAll('.bookmark_container');
+  // console.log(containersBookmark);
+
+  //Fazendo o loop para adicionar event listener em todos eles
+  containersBookmark.forEach((container, index) => {
+    container.addEventListener('click', () => {
+      console.log(`Container ${index} clicado`);
+
+      //! Caso o bookmark clicado seja da classe "Trending/emAlta" -->
+
+      if (container.parentElement.classList.contains('card_em_alta')) {
+        console.log(JSON.parse(sessionStorage.getItem('dados')));
+        atualizaNuvem(
+          container.parentElement.children[1].children[0].children[1]
+            .textContent
+        );
+        console.log(JSON.parse(sessionStorage.getItem('dados')));
+      } else if (container.parentElement.classList.contains('card_resultado')) {
+        atualizaNuvem(
+          container.parentElement.children[2].children[1].textContent
+        );
+      }
+    });
+  });
+}
+
+function atualizaNuvem(nomeDoPrograma) {
+  //todo Função que é chamada toda vez que o um container do bookMark é clicado, irá mudar o estado "isBookmarking" do objeto correspondente ao título do bookMark clicado.
+
+  dados = JSON.parse(sessionStorage.getItem('dados'));
+  dados.map((elemento, index) => {
+    if (elemento.title === nomeDoPrograma) {
+      //Faz a verificação do estado atual da propriedade isBookmarked do elemento e a muda.
+      elemento.isBookmarked === true
+        ? (elemento.isBookmarked = false)
+        : (elemento.isBookmarked = true);
+    }
+  });
+  sessionStorage.setItem('dados', JSON.stringify(dados));
+  // sessionStorage.setItem(JSON.stringify('dados', dados));
 }
