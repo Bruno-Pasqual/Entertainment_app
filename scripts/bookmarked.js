8d33609609bd81b578746654ceb03d0da4320b4a
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-01-19 23:02:11 -0300

    a

--- scripts/bookmarked.js
+++ scripts/bookmarked.js
@@ -10,6 +10,7 @@ function comecaScript() {
   atualizaArrayFilmesETv();
   criaOsCards();
   habilitaBookmarks();
+  pesquisaEscondeEMostra();
 }
 
 //! -------------------
@@ -52,7 +53,9 @@ function criaOsCards() {
         <ul>
           <li>
             <img
-              src="./assets/icon-category-tv.svg"
+              src="./assets/icon-category-${
+                objeto.category === 'Movie' ? 'movie' : 'tv'
+              }.svg"
               alt=""
               class="category_icon"
             />
@@ -78,7 +81,9 @@ function criaOsCards() {
         <ul>
           <li>
             <img
-              src="./assets/icon-category-tv.svg"
+              src="./assets/icon-category-${
+                objeto.category === 'Movie' ? 'movie' : 'tv'
+              }.svg"
               alt=""
               class="category_icon"
             />
@@ -104,10 +109,8 @@ function criaOsCards() {
   });
 }
 
-function atualizaTodosBookmarks() {}
-
+//! Selecionando os elementos ---------
 function habilitaBookmarks() {
-  //! Selecionando os elementos ---------
   let bookmarkContainers = document.querySelectorAll('.bookmark_container');
   let bookmarkIcon = document.querySelectorAll('.bookmark_icon');
   let containerTitles = document.querySelectorAll('.card_title');
@@ -135,4 +138,81 @@ function mudaEAtualizaNuvem(containerTitle) {
   comecaScript();
 }
 
-//! Estou tentando utilizar o título do container para acha-lo na variável dados, alterar o seu estado e depois atualizar a "nuvem" ainda não deu certo. !!
+function pesquisaEscondeEMostra() {
+  //! Selecionando os elementos --
+  let searchInput = document.getElementById('search_input');
+  let containerPaiFilmes = document.getElementById('container_pai_filmes');
+  let containerPaiTV = document.getElementById('container_pai_tv');
+  let containerResultadoPesquisa = document.getElementById(
+    'container_resultado_pesquisa'
+  );
+  //! Selecionando os elementos --
+
+  searchInput.addEventListener('input', () => {
+    console.log(searchInput.value.length != 0);
+    if (searchInput.value.length !== 0) {
+      containerPaiFilmes.style.display = 'none';
+      containerPaiTV.style.display = 'none';
+      containerResultadoPesquisa.style.display = 'flex';
+      mostraResultadoPesquisa(searchInput.value);
+    } else {
+      containerPaiFilmes.style.display = 'block';
+      containerPaiTV.style.display = 'block';
+      containerResultadoPesquisa.style.display = 'none';
+    }
+  });
+}
+
+function mostraResultadoPesquisa(stringPesquisada) {
+  const containerCardsPesquisa = document.getElementById(
+    'container_pesquisa_cards'
+  );
+  let bookmarkedShows = [...arrayFilmes, ...arrayTv];
+  let showsCorrespondem = bookmarkedShows.filter((objeto) => {
+    return objeto.title.toLowerCase().includes(stringPesquisada.toLowerCase())
+      ? true
+      : false;
+  });
+
+  //Criando os cards e alterando as informações
+  containerCardsPesquisa.innerHTML = '';
+  showsCorrespondem.map((objeto) => {
+    containerCardsPesquisa.innerHTML += `<div class="card_resultado">
+    <div class="bookmark_container">
+      <img src="./assets/icon-bookmark-${
+        objeto.isBookmarked ? 'full' : empty
+      }.svg" alt="" class="bookmark_icon" />
+    </div>
+    <div class="container_img_pesquisa"></div>
+    <div class="container_informacoes_recomendado">
+      <div class="first_row">
+        <p class="card_year">${objeto.year}</p>
+        <ul>
+          <li>
+            <img
+              src="./assets/icon-category-${
+                objeto.category === 'Movie' ? 'movie' : 'tv'
+              }.svg"
+              alt=""
+              class="category_icon"
+            />
+            <p class="category_name">${objeto.category}</p>
+          </li>
+          <li><p class="classification">${objeto.rating}</p></li>
+        </ul>
+      </div>
+      <p class="card_title">${objeto.title}</p>
+    </div>
+  </div>`;
+  });
+
+  let cardsResultados = document.querySelectorAll('.container_img_pesquisa');
+  console.log(cardsResultados);
+
+  //Atualizando as imagens em todos os cards criados
+  showsCorrespondem.map((objeto, index) => {
+    cardsResultados[
+      index
+    ].style.backgroundImage = `url("${objeto.thumbnail.regular.small}")`;
+  });
+}
---
+++ scripts/tvseries.js
@@ -0,0 +1,106 @@
+dados = [];
+tvseries = [];
+
+//!Declarando as funções ----------------------------------
+
+function comecaScript() {
+  dados = JSON.parse(sessionStorage.getItem('dados'));
+
+  criaCards();
+  atualizaBookmarks();
+  atualizaBookmarkClicado();
+}
+
+function criaCards() {
+  let containerPai = document.getElementById('container_cards_recomendados');
+  // ---
+  tvseries = dados.filter((objeto) => {
+    return objeto.category !== 'Movie';
+  });
+
+  //! Criando os cards e ja usando o array de objeto "tvseries" para alterar suas informações
+  tvseries.map((objeto) => {
+    containerPai.innerHTML += `<div class="card_resultado">
+    <div class="bookmark_container">
+      <img src="./assets/icon-bookmark-empty.svg" alt="" class="bookmark_icon" />
+    </div>
+    <div class="container_imagem_resultado"></div>
+    <div class="container_informacoes_recomendado">
+      <div class="first_row">
+        <p class="card_year">${objeto.year}</p>
+        <ul>
+          <li>
+            <img
+              src="./assets/icon-category-${
+                objeto.category === 'Movie' ? 'movie' : 'tv'
+              }.svg"
+              alt=""
+              class="category_icon"
+            />
+            <p class="category_name">${objeto.category}</p>
+          </li>
+          <li><p class="classification">${objeto.rating}</p></li>
+        </ul>
+      </div>
+      <p class="card_title">${objeto.title}</p>
+    </div>
+  </div>`;
+  });
+
+  //Selecionando as divs que contêm as imagens dos cards e as alterando com a imagem correspondente.
+  let cards = document.querySelectorAll('.container_imagem_resultado');
+  tvseries.map((objeto, index) => {
+    cards[
+      index
+    ].style.backgroundImage = `url("${objeto.thumbnail.regular.small}")`;
+  });
+}
+
+function atualizaBookmarks() {
+  let bookmarkIcons = document.querySelectorAll('.bookmark_icon');
+  tvseries.map((objeto, index) => {
+    if (objeto.isBookmarked === true) {
+      bookmarkIcons[index].src = `./assets/icon-bookmark-full.svg`;
+    } else {
+      bookmarkIcons[index].src = `./assets/icon-bookmark-empty.svg`;
+    }
+  });
+}
+
+function atualizaBookmarkClicado() {
+  //! Selecionando os elementos necessários ---------------------------------
+
+  let bookmarkContainers = document.querySelectorAll('.bookmark_container');
+  let bookmarkIcons = document.querySelectorAll('.bookmark_icon');
+
+  //! Realizando a lógica ----------------
+
+  bookmarkContainers.forEach((container, index) => {
+    container.addEventListener('click', () => {
+      if (tvseries[index].isBookmarked === true) {
+        bookmarkIcons[index].src = `/assets/icon-bookmark-empty.svg`;
+        atualizaDados(tvseries[index].title);
+      } else {
+        bookmarkIcons[index].src = `./assets/icon-bookmark-full.svg`;
+        atualizaDados(tvseries[index].title);
+      }
+    });
+  });
+}
+
+comecaScript();
+
+function atualizaDados(strTitulo) {
+  //todo Função que irá atualizar o array dados e o salvará no sessionStorage
+
+  dados.map((objeto, index) => {
+    if (objeto.title === strTitulo) {
+      if (dados[index].isBookmarked === true) {
+        dados[index].isBookmarked = false;
+      } else {
+        dados[index].isBookmarked = true;
+      }
+    }
+  });
+  sessionStorage.setItem('dados', JSON.stringify(dados));
+}
